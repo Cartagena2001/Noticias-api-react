216298db3538c5fd50add273b5f6fffaf5a8856f
Cartagena2001 <salvadorcartajena@hotmail.com>
2022-07-26 17:48:31 -0600

    :bug: bug

--- src/components/ListaNoticias.jsx
+++ src/components/ListaNoticias.jsx
@@ -4,7 +4,7 @@ import PropTypes from "prop-types";
 
 const ListaNoticias = ({noticias}) => (
   <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 justify-items-center gap-5 mt-10">
-    {noticias.map((noticia) => (
+    {noticias.map(noticia => (
       <Noticia key={noticia.url} noticia={noticia} />
     ))}
   </div>
